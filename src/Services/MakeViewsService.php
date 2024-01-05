2bb1b966094263c638c6d41137dedb9b934d9030
Moch Gani Setiawan <mochgani@gmail.com>
2024-01-05 16:47:37 +0700

    new type enum

--- src/Services/MakeViewsService.php
+++ src/Services/MakeViewsService.php
@@ -152,10 +152,10 @@ class MakeViewsService
                     $enum_val .= '\'' . $val . '\' => \'' . $val .'\', ';
                 }
                 // our placeholders
-                $formEdit .=str_repeat("\t", 2).'<div class="mb-3">'."\n";
-                $formEdit .=str_repeat("\t", 3).'{{ Form::label(\''.trim($column).'\', \''.ucfirst(trim($column)).'\', [\'class\'=>\'form-label\']) }}'."\n";
-                $formEdit .=str_repeat("\t", 3).'{{ Form::select(\''.trim($column).'\',array(\''.substr($enum_val,0,-2).'\'), null, array(\'class\' => \'form-control\')) }}'."\n";
-                $formEdit .=str_repeat("\t", 2).'</div>'."\n";
+                $formCreate .=str_repeat("\t", 2).'<div class="mb-3">'."\n";
+                $formCreate .=str_repeat("\t", 3).'{{ Form::label(\''.trim($column).'\', \''.ucfirst(trim($column)).'\', [\'class\'=>\'form-label\']) }}'."\n";
+                $formCreate .=str_repeat("\t", 3).'{{ Form::select(\''.trim($column).'\',array(\''.substr($enum_val,0,-2).'\'), null, array(\'class\' => \'form-control\')) }}'."\n";
+                $formCreate .=str_repeat("\t", 2).'</div>'."\n";
             } else {
                 // our placeholders
                 $formCreate .=str_repeat("\t", 2).'<div class="mb-3">'."\n";
